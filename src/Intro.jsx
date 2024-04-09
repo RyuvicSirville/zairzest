44042dff38807dbf418d124b09b1402c2cfd7115
RyuvicSirville <rsogbimal9@gmail.com>
2024-04-09 13:16:57 +0530

    fin1

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -10,10 +10,10 @@ export function Intro() {
                 <div class="cols-start-1 cols-end-2">
                     <div class="grid grid-rows-3 max-h-[800px]">
                         <div class=" py-[50px] px-[100px] row-start-1 row-end-3">
-                            <div class="text-gray-100 font-sans text-[4vw]">Enter into Digital Playground with</div>
+                            <div class="text-gray-100 font-sans text-[min(4vw,3.8vh)]">Enter into Digital Playground with</div>
 
-                            <img class="h-[2vw]" src={zaiza}></img>
-                            <div class=" py-10 text-gray-100 font-sans text-[2vw]">
+                            <img class="h-[min(2vw,3.5vh)]" src={zaiza}></img>
+                            <div class=" py-10 text-gray-100 font-sans text-[min(2vw,3.5vh)]">
                                 <div>Release all your stress with the exciting Tech and Fun events in the most awaited fest . Zairzest 2.0 presented by Zairza. </div>
                                 <div></div>
                                 <button onClick={() => { navigate("/signup") }} class=" row-start-4"><img class="h-[2vw]" src={registern}></img ></button>
